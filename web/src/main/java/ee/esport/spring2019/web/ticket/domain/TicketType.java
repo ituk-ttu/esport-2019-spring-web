5cf217d310a9fba70c914c5f908260a99050cd26
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2019-01-25 17:29:33 +0200

    Offering card

--- web/src/main/java/ee/esport/spring2019/web/ticket/domain/TicketType.java
+++ web/src/main/java/ee/esport/spring2019/web/ticket/domain/TicketType.java
@@ -4,10 +4,6 @@ import lombok.Builder;
 import lombok.NonNull;
 import lombok.Value;
 
-import java.math.BigDecimal;
-import java.time.OffsetDateTime;
-import java.util.List;
-
 @Value
 @Builder
 public class TicketType {
@@ -15,25 +11,8 @@ public class TicketType {
     @NonNull private final Integer id;
     @NonNull private final String code;
     @NonNull private final Integer amountAvailable;
-    @NonNull private final Integer amountActive;
+    @NonNull private final Integer amountRemaining;
     @NonNull private final Integer teamSize;
     @NonNull private final Boolean assignedSeating;
-    @NonNull private final List<Offering> offerings;
-
-    @Value
-    @Builder
-    public static class Offering {
-
-        @NonNull private final Integer id;
-        @NonNull private final String name;
-        private final Integer amountAvailable;
-        @NonNull private final Integer amountActive;
-        private final OffsetDateTime availableFrom;
-        private final OffsetDateTime availableUntil;
-        @NonNull private final Boolean availableOnline;
-        @NonNull private final Boolean promotional;
-        @NonNull private final BigDecimal cost;
-
-    }
 
 }
