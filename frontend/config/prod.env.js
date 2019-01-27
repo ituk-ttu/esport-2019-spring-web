a6e755d66c62e10dada6af43033bb4c56da15dda
Jaagup Kümmel <jaagup.kymmel@nortal.com>
2019-01-27 18:34:30 +0200

    Use dev conf in live

--- frontend/config/prod.env.js
+++ frontend/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = {
-  NODE_ENV: '"production"',
+  NODE_ENV: '"development"',
   apiBase: '"https://api.esport.ituk.ee"',
   steamLoginReturnTo: '"https://e-sport.ee/static/verifySteamLogin.html"'
 };
